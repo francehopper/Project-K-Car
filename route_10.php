f7dcae423fc0378641648a828b6bbec133d0a24b
Stephen J. Weber <francehopper@gmail.com>
2014-08-13 18:09:00 -0400

    Magic

--- route_10.php
+++ route_10.php
@@ -3,47 +3,14 @@
 <head>
 	<title>K-Car > Trolley Route 10</title>
 	<!-- Get Google Maps API as an experiment -->
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["map"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Lat', 'Long', 'Name'],
-          [37.4232, -122.0853, 'Work'],
-          [37.4289, -122.1697, 'University'],
-          [37.6153, -122.3900, 'Airport'],
-          [37.4422, -122.1731, 'Shopping']
-        ]);
 
-        var map = new google.visualization.Map(document.getElementById('map_div'));
-        map.draw(data, {showTip: true});
-      }
-
-    </script>
 
 
 </head>
 <body>
 <?php
 // connect to SEPTA to get vehicle locations
-// create a new cURL resource
-// $ch = curl_init();
-
-// // set URL and other appropriate options
-// curl_setopt($ch, CURLOPT_URL, "http://www3.septa.org/hackathon/TransitViewAll/'");
-// curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// // grab URL and pass it to the browser
-// curl_exec($ch);
-
-// close cURL resource, and free up system resources
-
 $json = file_get_contents('http://www3.septa.org/hackathon/TransitView/10');
-// $json = "http://www3.septa.org/hackathon/TransitViewAll/";
-
-// you can save $json to a file, if needed :
-//file_put_contents('file/path/my-file.txt', $json);
 
 // get data from JSON feed
 $data = json_decode($json, true);
@@ -83,5 +50,23 @@ echo '<div id="map_div" style="width: 400px; height: 300px"></div>'
 // }
 
 ?>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["map"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Lat', 'Long', 'Name'],
+          [37.4232, -122.0853, 'Work'],
+          [37.4289, -122.1697, 'University'],
+          [37.6153, -122.3900, 'Airport'],
+          [37.4422, -122.1731, 'Shopping']
+        ]);
+
+        var map = new google.visualization.Map(document.getElementById('map_div'));
+        map.draw(data, {showTip: true});
+      }
+
+    </script>
 </body>
 </html>
