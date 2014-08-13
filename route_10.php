ed82870dc7e9e2621cf6ce3fcafa7502b90e6a3f
Stephen J. Weber <stephen@n1-37-92.dhcp.drexel.edu>
2014-08-13 13:42:36 -0400

    Maps Experiment Start

--- route_10.php
+++ route_10.php
@@ -1,7 +1,26 @@
 <!doctype html>
 <html>
 <head>
-<title>K-Car > Trolley Route 10</title>
+	<title>K-Car > Trolley Route 10</title>
+	<!-- Get Google Maps API as an experiment -->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["map"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Lat', 'Long', 'Name'],
+          [37.4232, -122.0853, 'Work'],
+          [37.4289, -122.1697, 'University'],
+          [37.6153, -122.3900, 'Airport'],
+          [37.4422, -122.1731, 'Shopping']
+        ]);
+
+        var map = new google.visualization.Map(document.getElementById('map_div'));
+        map.draw(data, {showTip: true});
+      }
+
+    </script>
 
 
 </head>
@@ -54,6 +73,7 @@ foreach ($data['bus'] as $line) { // parse the returned data
 // curl_close($ch);
 
 // start Google Maps experiment
+<div id="map_div" style="width: 400px; height: 300px"></div>
 
 
 
