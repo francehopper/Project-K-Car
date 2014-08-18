0d2a809e4a3224578a955863ac416986bb788927
Stephen J. Weber <francehopper@gmail.com>
2014-08-18 19:45:22 -0400

    Synced code across routes except for 10
    Since route 10 is special

--- route_34.php
+++ route_34.php
@@ -1,46 +1,19 @@
 <!doctype html>
 <html>
 <head>
-<title>K-Car > Trolley Route 34</title>
-
-
+	<title>K-Car > Trolley Route 34</title>
 </head>
 <body>
 <?php
+// open an SQL connection
+$connection = mysqli_connect('totoro.hppr.co:3308', 'SEPTA', 'D1oASa!', 'SEPTA') or die(mysqli_connect_error());
+date_default_timezone_set('UTC'); // set timezone as UTC
+$today = date("Y-m-d H:i:s"); // fetch current date and time for SQL timestamping
 // connect to SEPTA to get vehicle locations
-// create a new cURL resource
-// $ch = curl_init();
-
-// // set URL and other appropriate options
-// curl_setopt($ch, CURLOPT_URL, "http://www3.septa.org/hackathon/TransitViewAll/'");
-// curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// // grab URL and pass it to the browser
-// curl_exec($ch);
-
-// close cURL resource, and free up system resources
-
 $json = file_get_contents('http://www3.septa.org/hackathon/TransitView/34');
-// $json = "http://www3.septa.org/hackathon/TransitViewAll/";
-
-// you can save $json to a file, if needed :
-//file_put_contents('file/path/my-file.txt', $json);
-
 // get data from JSON feed
 $data = json_decode($json, true);
-
-// DEBUG: dump fetched data
-// echo "<p>Doing var_dump</p><br/>";
-// var_dump($data); // works
-
-// http://stackoverflow.com/questions/17995877/get-value-from-json-array-in-php
-// debug
-// echo "<p>Doing echo</p><br/>";
-// foreach($data['bus'] as $result) {
-//     echo $result['Direction'], '<br />';
-// }
-
-
+// begin data processing
 foreach ($data['bus'] as $line) { // parse the returned data
     $lat = $line['lat']; // get lat value
     $lng =  $line['lng']; // get lng value
@@ -50,15 +23,11 @@ foreach ($data['bus'] as $line) { // parse the returned data
     $lastReport = $line['Offset']; // get minutes since last location report
     $message = '<li> Vehicle No. '.$vehicleNo.' heading '.$direction.' to '.$goingTo.' was last seen '.$lastReport.' minutes ago at '.$lat.' latitude and '.$lng.' longitute.';
     echo $message; // return the status
+    // record results to SQL for tracking
+    $thequery = 'insert into KCar (TrolleyNo, dest, lat, lng, direction, recordedAt) values ("'.$vehicleNo.'","'.$goingTo.'","'.$lat.'","'.$lng.'","'.$direction.'","'.$today.'")';
+	mysqli_query($connection, $thequery) or die (mysqli_error($connection));
 }
-// curl_close($ch);
-
-
-
-// '<li>'.$status.'</span> <a style="font-size:85%" href="http://twitter.com/'.$userName.'/statuses/'.$tweetId.'">'. $tweetTime .'</a></li>'; // Render our beautiful new tweet
-//     echo $outputTweet; // echo the tweet
-// }
-
+// END OF PHP!
 ?>
 </body>
 </html>
